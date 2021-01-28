18b479590601789f4f245febac18f72dbb94cfe3
gayara <gayarakumari366@gmail.com>
2021-01-28 23:43:00 +0530

    Calculator

---
+++ app/src/main/java/com/example/cal/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button Cal;
+
+        Cal=(Button)findViewById(R.id.button2);
+
+        Cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+          Intent myIntent=new Intent(MainActivity.this,Calculator.class);
+          MainActivity.this.startActivity(myIntent);
+            }
+
+        });
+    }
+}
